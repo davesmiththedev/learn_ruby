ce22314044742a818b01d3ab8b0ff9c36d6fe63b
Dave Smith <lah4life@gmail.com>
2016-07-01 22:21:10 -0700

    solve 09_timer

---
+++ 09_timer/timer.rb
@@ -0,0 +1,35 @@
+class Timer
+	attr_reader :seconds
+
+	def initialize(seconds=0)
+	  @seconds = seconds
+	end
+
+	def seconds= seconds
+		@seconds = seconds
+	end
+
+	def time_string
+		#Calc and format seconds
+		seconds = padded(@seconds % 60)
+		
+		#Calc and format minutes
+		minutes = padded((@seconds / 60) % 60)
+
+		#Calc and format hours
+		hours = padded((@seconds / 60) / 60)
+
+		#return the merged results
+		hours.to_s + ":" + minutes.to_s + ":" + seconds.to_s
+	end
+
+	def padded number
+		number = number.to_s
+		if (number.length < 2)
+			#pad with a 0
+			number = "0"+number
+		end
+		#return the number
+		number
+	end
+end
