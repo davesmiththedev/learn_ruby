ece8b1991314bfb4f2e7bd7465522c4bf5cb3b48
Dave Smith <lah4life@gmail.com>
2016-07-01 18:35:20 -0700

    solve 08_book_titles

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -1,3 +1,37 @@
-def title name
-	name = name.capitalize
+class Book
+	attr_reader :title
+
+	def title= title
+		@title = capitalize(title)
+	end
+
+	def capitalize title
+		
+		words = title.split(' ')
+		
+		words.each do |word|
+			if words.index(word) == 0
+				word.capitalize!
+			else
+				if word == 'and'
+					#'cunjunction'
+					word.downcase!
+				elsif ['in', 'of'].include?(word)
+					#'preposition'
+					word.downcase!
+				elsif ['a', 'the', 'an'].include?(word)
+					#'article'
+					word.downcase!
+				else
+					#'other word'
+					word.capitalize!
+				end
+			end
+		end
+		
+		#combine to one string and remove trailing whitespace
+		words.join(' ').chomp
+
+	end
+
 end
